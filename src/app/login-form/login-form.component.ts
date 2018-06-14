210a076f51fb208b55e786037ec1eb0a1dbc8f11
Vridhi Sagar <vridhisagar@gmail.com>
2018-06-14 10:59:12 +0530

    test1

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -8,6 +8,8 @@ class login {
   public email:string="";
   public password:string="";
 }
+import {HttpClient} from "@angular/common/http";
+
 import {ServerService} from '../server.service';
 //import {DemoService} from '../demo-service/demo-service.component';
 import {forkJoin} from 'rxjs'; 
@@ -22,11 +24,13 @@ import {forkJoin} from 'rxjs';
 export class LoginFormComponent implements OnInit {
   _ServerService: any;
   model:login=new login();
+  message:string=""
+  clicked:boolean=false
   //loading= false;
   public email:string="";
   public password:string="";
   final:string="LOGIN";
-  constructor(private serv:ServerService  ) {}
+  constructor(private http:HttpClient  ) {}
    
   ngOnInit() {
     this.submit();
@@ -38,21 +42,23 @@ export class LoginFormComponent implements OnInit {
     )}
   
   submit(){
-    // console.log(this.model.email);
-    // console.log(this.model.password);
-    this._ServerService.submit().subscribe(
-        data=>
-        {
-             this.server();
-             return true;
+  //   // console.log(this.model.email);
+  //   // console.log(this.model.password);
+  //  this._ServerService.get.subscribe(
+  //       data=>
+  //       {
+  //            this.server();
+  //            return true;
 
-        }
+  //       }
 
 
 
-    )
+   // )
+   this.clicked=true;
    
-
+   console.log("you are successfully logged in");
+   this.message="You are successfully logged in";
 
   }
   

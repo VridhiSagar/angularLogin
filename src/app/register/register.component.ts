c136a9a9caef17ac466ad5694af21bef91176ba1
Vridhi Sagar <vridhisagar@gmail.com>
2018-06-25 14:48:15 +0530

    second commit

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Register} from '../registerUser';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+model: Register = new Register();
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/registerUser.ts
@@ -0,0 +1,11 @@
+export class Register {
+ public first: string;
+ public last: string;
+public email: string;
+public DOB: number;
+public gender: string;
+public contactNo: number;
+public password: string;
+}
+
+
